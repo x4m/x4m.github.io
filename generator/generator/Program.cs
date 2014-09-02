bcf560e17565f98e00ab7b7144fe848c3c24ccd1
Andrey Borodin <amborodin@acm.org>
2014-09-02 16:08:35 +0600

    v0.1

--- generator/generator/Program.cs
+++ generator/generator/Program.cs
@@ -13,17 +13,36 @@ namespace generator
         {
             var pattern = File.ReadAllText("..\\..\\..\\..\\pattern.html");
             var labFiles = Directory.GetFiles("..\\..\\..\\..\\Labs", "*.txt");
-            var labSections = labFiles.Select(f=>new{text = File.ReadAllText(f,Encoding.Default),caption = Path.GetFileNameWithoutExtension(f)}).Select(f=>FormatSection(f.caption,f.text));
+            var labSections = labFiles
+                .Select(f=>new{text = File.ReadAllText(f,Encoding.Default),caption = Path.GetFileNameWithoutExtension(f)})
+                .OrderBy(f=>f.caption)
+                .Select(f=>FormatLab(f.caption,f.text));
             var lecFiles = Directory.GetFiles("..\\..\\..\\..\\Lectures", "*.txt");
-            var lecSections = lecFiles.Select(f => new { text = File.ReadAllText(f, Encoding.Default), caption = Path.GetFileNameWithoutExtension(f) }).Select(f => FormatSection(f.caption, f.text));
+            var lecSections = lecFiles
+                .Select(f => new { text = File.ReadAllText(f, Encoding.Default), caption = Path.GetFileNameWithoutExtension(f) })
+                .OrderBy(f => f.caption)
+                .Select(f => FormatSection(f.caption, f.text));
             var output = //string.Format(pattern, string.Join("", labSections), string.Join("", lecSections));
                 pattern.Replace("{0}", string.Join("", labSections)).Replace("{1}", string.Join("", lecSections));
             File.WriteAllText("..\\..\\..\\..\\program.html", output);
         }
 
+        private static string FormatLab(string caption, string text)
+        {
+            var strings = text.Split(new []{Environment.NewLine},StringSplitOptions.RemoveEmptyEntries);
+
+            return "<div class=\"expandable\"><p>" + strings[0] + " <br>" + string.Join("<br/>",strings.Skip(1)) + "</p><br/></div>" + Environment.NewLine;
+        }
+
         private static string FormatSection(string caption, string text)
         {
-            return "<div class=\"expandable\"><p>" + caption + " <br>" + text.Replace(Environment.NewLine,"<br/>") + "</p><br/></div>" + Environment.NewLine;
+
+            return string.Format("<div class=\"expandable\"><p> Неделя {0} Занаятие {1} Тема {2} <br>{3}</p><br/></div>{4}", 
+                caption.Substring(0,2), 
+                caption.Substring(2,2), 
+                caption.Substring(5), 
+                text.Replace(Environment.NewLine,"<br/>"), 
+                Environment.NewLine);
         }
     }
 }
